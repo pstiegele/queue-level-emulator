f63788c74a251e6e80243b401aefa3c02fd3b2b3
pstiegele <paul@stiegele.name>
2020-06-03 14:54:41 +0200

    small clean up

--- task1_packet_forwarding/rust/src/main.rs
+++ task1_packet_forwarding/rust/src/main.rs
@@ -9,7 +9,7 @@ use std::time::Duration;
 
 use std::env;
 
-// Invoke as echo <interface name>
+
 fn main() {
 
     thread::spawn(|| {
@@ -32,7 +32,6 @@ fn forward(receivingInterface:String, sendingInterface:String, srcMac:String, de
     let interface_names_match_rec =
         |iface: &NetworkInterface| iface.name == interface_name_rec;
 
-    // Find the network interface with the provided name
     let interfaces_rec = datalink::interfaces();
     let interface_rec = interfaces_rec.into_iter()
                               .filter(interface_names_match_rec)
@@ -46,7 +45,6 @@ fn forward(receivingInterface:String, sendingInterface:String, srcMac:String, de
     let interface_names_match_send =
         |iface: &NetworkInterface| iface.name == interface_name_send;
 
-    // Find the network interface with the provided name
     let interfaces_send = datalink::interfaces();
     let interface_send = interfaces_send.into_iter()
                             .filter(interface_names_match_send)
@@ -74,25 +72,6 @@ fn forward(receivingInterface:String, sendingInterface:String, srcMac:String, de
                 //println!("packet received");
                 let packet_unwrapped = EthernetPacket::new(packet).unwrap();
 
-                // Constructs a single packet, the same length as the the one received,
-                // using the provided closure. This allows the packet to be constructed
-                // directly in the write buffer, without copying. If copying is not a
-                // problem, you could also use send_to.
-                //
-                // The packet is sent once the closure has finished executing.
-                // let interface_name = &sendingInterface;
-                // let interface_names_match =
-                //     |iface: &NetworkInterface| iface.name == *interface_name;
-            
-                // // Find the network interface with the provided name
-                // let interfaces = datalink::interfaces();
-                // let interface = interfaces.into_iter()
-                //                           .filter(interface_names_match)
-                //                           .next()
-                //                           .unwrap();
-                //if(packet_unwrapped.get_destination()=="123"){
-                    //println!("destination: {}", packet_unwrapped.get_destination().to_string());
-                    //println!("source: {}", packet_unwrapped.get_source().to_string());
 
                 if(packet_unwrapped.get_source().to_string()==srcMac){
                     if(packet_unwrapped.get_destination().to_string()==destMac||packet_unwrapped.get_destination().to_string()=="ff:ff:ff:ff:ff:ff"){
@@ -100,21 +79,8 @@ fn forward(receivingInterface:String, sendingInterface:String, srcMac:String, de
                     }
 
                 }
-                    
-                //}
+                                    
                 
-
-                // tx.build_and_send(1, packet.packet().len(),
-                //     &mut |mut new_packet| {
-                //         let mut new_packet = MutableEthernetPacket::new(new_packet).unwrap();
-
-                //         // Create a clone of the original packet
-                //         new_packet.clone_from(&packet);
-
-                //         // Switch the source and destination
-                //         new_packet.set_source(packet.get_destination());
-                //         new_packet.set_destination(packet.get_source());
-                // });
             },
             Err(e) => {
                 // If an error occurs, we can handle it here
